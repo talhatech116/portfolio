80a6db807f5241ce184d0f8eca49e3d98c1144a5
Talha Sahni <muhammad.abdullah.1230221@gmail.com>
2023-11-29 15:05:16 +0500

    Your commit message

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './components/header/Header';
+import Intro from './components/intro/intro';
 
 const App = () => {
   return (
     <>
       <Header />
+      <Intro />
     </>
   )
 }
---
+++ src/components/intro/intro.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { FaAward } from 'react-icons/fa';
+import { VscFolderLibrary } from 'react-icons/vsc';
+import ME from '../../assets/talha.jpg';
+import './intro.css';
+
+const Intro = () => {
+  return (
+    <section id="about">
+      <h5>Get to know</h5>
+      <h2>About Me</h2>
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="me" />
+          </div>
+        </div>
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FaAward className="about__icon" />
+              <h5>Experience</h5>
+              <small>1+ year</small>
+            </article>
+            <article className="about__card">
+              <VscFolderLibrary className="about__icon" />
+              <h5>Projects</h5>
+              <small>10+ Completed Projects</small>
+            </article>
+          </div>
+          <p>I have over a year of experience developing web applications and software as a professional Laravel developer. I have experience developing RESTful APIs and providing reliable backend solutions. I am skilled in HTML, CSS, JavaScript, Bootstrap, and Ajax on the front end, which enables me to develop fluid and interactive user interfaces. I am committed to providing top-notch, user-centric solutions that have an impact over time.</p>
+          <a href="#contact" className="btn btn-primary">Let's Talk</a>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Intro
